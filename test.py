b93eb1a3387f0acfc6a8545079089804ec77e79f
Katarzyna Kulagowska <kkulagow@c2r3s4.42warsaw.pl>
2026-02-26 22:11:59 +0100

    Added class Window()

--- test.py
+++ test.py
@@ -1,22 +1,30 @@
 from mlx import Mlx
 
-def close(param):
-    
 
+class Window():
 
+    def __init__(self):
+        self.mlx = Mlx()
+        self.ptr = self.mlx.mlx_init()
+        self.window = self.mlx.mlx_new_window(
+            self.ptr, 800, 800, "whoores"
+            )
+        self.mlx.mlx_pixel_put(self.ptr, self.window, 200, 200, 0xFFFF00)
+        self.mlx.mlx_hook(self.window, 33, 0, self.close, None)
+        self.mlx.mlx_key_hook(self.window, self.key_event, None)
 
+    def key_event(self, key, param):
+        if key == 65307:  #bash xav do sprawdzenia
+            self.close(None)
 
-def main():
-    mlx = Mlx()
-    ptr = mlx.mlx_init()
+    def close(self, param):
+        self.mlx.mlx_destroy_window(self.ptr, self.window)
+        self.mlx.mlx_loop_exit(self.ptr) #zamyka okno
 
-    window = mlx.mlx_new_window(ptr, 100, 100, "whoores")
-    rzeczy = {
-        "ptr": mlx,
-        "window": window,
-    }
-    
-    mlx.mlx_loop(ptr)
+    def show(self):
+        self.mlx.mlx_loop(self.ptr)
+
 
 if __name__ == "__main__":
-    main()
+    window = Window()
+    window.show()
