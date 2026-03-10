2ed89cdaab3892de70ba05a44ce49e3e22907ecf
Katarzyna Kulagowska <kkulagow@c3r12s2.42warsaw.pl>
2026-03-10 20:49:02 +0100

    Cleaned maze

--- Window.py
+++ Window.py
@@ -30,13 +30,14 @@ class Window():
 
         file = open("output.txt")
         self.lines = file.readlines()
-        width_img = len(self.lines[0]) - 1
-        height_img = len(self.lines) - 4
+        self.width_img = len(self.lines[0]) - 1
+        self.height_img = len(self.lines) - 4
         self.size = 32
-        self.img_ptr = self.mlx.mlx_new_image(self.ptr, width_img * self.size, height_img * (self.size)) # 800 to piksele czyli jeden piksel to 4 bity czyli jeden bajt
+        self.img_ptr = self.mlx.mlx_new_image(self.ptr, self.width_img * self.size, (self.height_img + 1) * (self.size - 1)) # 800 to piksele czyli jeden piksel to 4 bity czyli jeden bajt
         self.mlx_img_data = self.mlx.mlx_get_data_addr(self.img_ptr)
-        print(width_img)
-        print(height_img)
+        print(self.width_img)
+        print(self.height_img)
+        print(f"Dlugosc obrazu: {(self.height_img + 1) * (self.size - 1)}")
         self.image_data = self.mlx_img_data[0]
         self.line_length = self.mlx_img_data[2]
         # self.image_data[:] = bytes([0, 0, 0, 255]) * (len(self.image_data)//4)
@@ -54,11 +55,10 @@ class Window():
             self.close(None)
     
     def draw_pixel(self, pixel = None):
-        first_line = self.lines[0]
-        for line in self.lines[:-3]:
+        for line in self.lines[:-4]:
+            self.x = 0
             for px in line[:-1]:
                 pixel = format(int(px,16),"04b")
-                #print(pixel)
                 if pixel[0] == "1":
                     self.image_data[self.y*self.line_length + 4 * self.x: self.y*self.line_length + 4*(self.x + self.size)] = self.size * bytes([255,255,255,255])#gorna sciana
                 for i in range(0, self.size - 1):
@@ -66,21 +66,18 @@ class Window():
                         self.image_data[(self.y + i) * self.line_length + 4 * self.x: (self.y + i) * self.line_length + 4 *(self.x + 1)] = bytes([255,255,255,255])#lewa sciana
                     if pixel[1] == "1":
                         self.image_data[(self.y + i) * self.line_length + 4* (self.x + self.size - 1): (self.y + i) * self.line_length + 4 *(self.x + self.size)] = bytes([255,255,255,255])#prawa sciana
+                        print(f"Prawa scianka : { (self.y + i) * self.line_length + 4 *(self.x + self.size)}")
                 if pixel[2] == "1":
                     self.image_data[(self.y+self.size - 1)*self.line_length + 4 * self.x: (self.y+ self.size - 1)*self.line_length + 4*(self.x + self.size)] = self.size * bytes([255,255,255,255])#dolna sciana
-                print("Lewa sciana:")
-                print((self.y + i) * self.line_length + 4 * self.x)
-                print("Prawa sciana:")
-                print((self.y + i) * self.line_length + 4* (self.x + self.size - 1))
+                    #print(f"Dolna scianka : {self.y+self.size - 1}")
+                if pixel == "1111":
+                     for i in range(1, self.size - 2):
+                         self.image_data[(self.y + i) * self.line_length + 4 * self.x: (self.y + i) * self.line_length + 4 *(self.x + self.size - 2)] = (self.size - 2) * bytes([255,255,255,50])
                 self.x += self.size  
             #break
-            self.y += self.size - 2
-        for line in horizontal_line:
-            for x in line:
-                if x == "1":
-                self.image_data[]
+            self.y += self.size - 1
         print(self.x)
-        print(self.y)
+        print(f"Y equals: {self.y}")
         self.mlx.mlx_put_image_to_window( self.ptr, self.window, self.img_ptr, 560, 140)
     
     def draw_maze(self, param):
@@ -90,16 +87,16 @@ class Window():
             pixel = format(int(px,16),)
  
     def draw_window(self, param):
-        if self.i < 799 and self.z < 799:
-            self.image_data[0*self.line_length + 4*self.i:
-                            0*self.line_length + 4 * (self.i + 1)] = bytes([255,255,255,255])
-            self.image_data[(self.y) * self.line_length + 4*self.i:
-                            (self.y) * self.line_length + 4 * (self.i + 1)] = bytes([255,255,255,255])
+        #print((self.width_img * self.height_img - 2) * size)
+        #print(32 * (self.width_img * self.height_img - 2))
+        if self.i < self.x:
+            self.image_data[4*self.i: 4 * (self.i + 1)] = bytes([255, 255, 255, 255])
+            self.image_data[(self.y) * self.line_length + 4*self.i: (self.y) * self.line_length + 4 * (self.i + 1)] = bytes([255, 255, 255, 255])
         if self.z < self.y:
             self.image_data[self.z*self.line_length + 4*0:
-                            self.z*self.line_length + 4 * (0 + 1)] = bytes([255,255,255,255])
-            self.image_data[self.z*self.line_length + 4*798:
-                            self.z*self.line_length + 4 * (798 + 1)] = bytes([255,255,255,255])
+                            self.z*self.line_length + 4 * (0 + 1)] =  bytes([255, 255, 255, 255])
+            self.image_data[self.z*self.line_length + 4*799:
+                            self.z*self.line_length + 4 * (799 + 1)] = bytes([255, 255, 255, 255])
         self.mlx.mlx_put_image_to_window( self.ptr, self.window, self.img_ptr, 560, 140)
         self.i += 1 #temporary x
         self.z += 1 #temporary 
@@ -112,7 +109,7 @@ class Window():
     def show(self):
         self.draw_pixel()
         #self.mlx.mlx_loop_hook(self.ptr, self.draw_pixel, None)
-        #self.mlx.mlx_loop_hook(self.ptr, self.draw_window, None)
+        self.mlx.mlx_loop_hook(self.ptr, self.draw_window, None)
         #self.mlx.mlx_loop_hook(self.ptr, schlang(self.mlx, self.window, self.ptr), self)
         self.mlx.mlx_loop(self.ptr)
 
