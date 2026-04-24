91dabbb0942ca63e5cf7f96a7e280580b78c574a
Katarzyna Kulagowska <kkulagow@c2r4s3.42warsaw.pl>
2026-04-24 19:35:42 +0200

    Flake and mypy corrected

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,4 +1,7 @@
-def load_config(file_name: str) -> any:
+from typing import Any
+
+
+def load_config(file_name: str) -> Any:
     config = {}
 
     with open(file_name, "r") as f:
@@ -11,11 +14,13 @@ def load_config(file_name: str) -> any:
             key, value = line.split("=", 1)
             config[key.strip()] = value.strip()
 
-    required_keys = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"]
+    required_keys = ["WIDTH", "HEIGHT", "ENTRY",
+                     "EXIT", "OUTPUT_FILE", "PERFECT"]
 
     missing = [key for key in required_keys if key not in config]
     if missing:
-        raise ValueError(f"Brak wymaganych pól w configu: {', '.join(missing)}")
+        raise ValueError(
+            f"Brak wymaganych pól w configu: {', '.join(missing)}")
 
     width = int(config["WIDTH"])
     height = int(config["HEIGHT"])
@@ -24,14 +29,17 @@ def load_config(file_name: str) -> any:
     entry = (int(x), int(y))
     x, y = config["EXIT"].split(",")
     exit = (int(x), int(y))
-    
+
     output_file = config["OUTPUT_FILE"]
 
     perfect = config["PERFECT"].lower() == "true"
 
     return width, height, entry, exit, output_file, perfect
 
-def validate_config(width: int, height: int, entry: tuple, exit: tuple, output_file: str, perfect: bool):
+
+def validate_config(width: int, height: int, entry: tuple,
+                    exit: tuple, output_file: str, perfect: bool
+                    ) -> None:
     if (width < 10):
         raise ValueError("Width must be greater than 9")
     if (height < 10):
@@ -44,12 +52,11 @@ def validate_config(width: int, height: int, entry: tuple, exit: tuple, output_f
         raise ValueError("Entry and exit cannot be the same")
     if not output_file.endswith(".txt"):
         raise ValueError("Output file must have a .txt extension")
-    if perfect != True and perfect != False:
+    if perfect is not True and perfect is not False:
         raise ValueError("Is perfect must be true or false")
+
 
 if __name__ == "__main__":
     config_name = "config.txt"
     width, height, entry, exit, output_file, perfect = load_config(config_name)
     validate_config(width, height, entry, exit, output_file, perfect)
-
-    
